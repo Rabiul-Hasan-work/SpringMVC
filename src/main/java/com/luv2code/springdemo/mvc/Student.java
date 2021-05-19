de61df9e5c0401fa460bd5590f4a9ea9ff48809f
Rabiul Hasan <rabiulworkapps@gmail.com>
2021-05-19 16:36:19 +0600

    Spring MVC Dropdown list

--- src/main/java/com/luv2code/springdemo/mvc/Student.java
+++ src/main/java/com/luv2code/springdemo/mvc/Student.java
@@ -5,12 +5,22 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	
+	private String country;
+	
 	public Student() {
 		
 	}
 
 	public String getFirstName() {
 		return firstName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
 	}
 
 	public void setFirstName(String firstName) {
