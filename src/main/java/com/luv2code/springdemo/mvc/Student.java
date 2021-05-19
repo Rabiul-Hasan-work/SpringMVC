826727e1b9b6cacb5134343aa58c6beba99d12ed
Rabiul Hasan <rabiulworkapps@gmail.com>
2021-05-19 17:07:52 +0600

    Dropdown Menu with java instead jsp

--- src/main/java/com/luv2code/springdemo/mvc/Student.java
+++ src/main/java/com/luv2code/springdemo/mvc/Student.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	
 	private String firstName;
@@ -7,8 +9,22 @@ public class Student {
 	
 	private String country;
 	
+	private LinkedHashMap<String, String> countryOptions;
+	
 	public Student() {
 		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States of America");
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 
 	public String getFirstName() {
